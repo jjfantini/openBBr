a8a37d29196ee98032205524c30c9e9be3b5718a
jj fantini <jjfantini@gmail.com>
2023-05-19 16:58:43 -0400

    change: added .onLoad function

---
+++ zzz.R
@@ -0,0 +1,7 @@
+# .onLoad <- function(libname, pkgname) {
+#     # Load the openbb library
+#     reticulate::py_run_string("from openbb_terminal.sdk import openbb")
+
+#     # Delay the loading of the numpy and pandas
+
+# }
